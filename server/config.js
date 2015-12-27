06ee744ee26d456b6ac6cd35a8015e7d5806b1e0
Ryan Hamblin <ryanhca@gmail.com>
2015-12-27 00:37:10 -0600

    added backend

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	db: 'localhost',
-	clientSecret: proces.env.clientSecret || '412cdf8974cc4e72be969221fe2f3190',
-	tokenSecret: proces.env.tokenSecret || 'this will be our secret'
+	db: process.env.db || 'mongodb://localhost/27017',
+	clientSecret: process.env.clientSecret || '412cdf8974cc4e72be969221fe2f3190',
+	tokenSecret: process.env.tokenSecret || 'this will be our secret'
 };
